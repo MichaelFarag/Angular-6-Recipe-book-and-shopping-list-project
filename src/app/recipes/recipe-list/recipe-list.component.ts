a1816b07da186d56abfdb091388e13aad4808716
Michael Farag <MichaelsamyFarag@gmail.com>
2018-07-28 02:48:30 +0200

    adding recipe module wit logic and ingfor to list items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesModule } from '../recipes.module';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: RecipesModule[] = [
+    new RecipesModule('A Test Recipe 1', 'This a simple test descripatopn for recipe',
+      'https://cdn.pixabay.com/photo/2017/12/21/13/25/orange-3031789_960_720.jpg'),
+    new RecipesModule('A Test Recipe 2', 'This a simple test descripatopn for recipe',
+      'https://cdn.pixabay.com/photo/2017/12/21/13/25/orange-3031789_960_720.jpg')
+    new RecipesModule('A Test Recipe 3', 'This a simple test descripatopn for recipe',
+      'https://cdn.pixabay.com/photo/2017/12/21/13/25/orange-3031789_960_720.jpg')
+  ];
 
   constructor() { }
 
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class RecipesModule {
+
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+
+}
